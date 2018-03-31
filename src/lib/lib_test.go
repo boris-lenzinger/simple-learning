497cf3db9e83f49887dd2d81669318c565304376
Boris Lenzinger <boris.lenzinger@gmail.com>
2018-03-31 11:13:13 +0200

    Adding test to cover random interrogation.
    This test is quite simple and checks that the random mode is simply not
    the linear mode. Basic but efficient.

--- src/lib/lib_test.go
+++ src/lib/lib_test.go
@@ -251,6 +251,43 @@ func getGenericInteractiveInterrogationParameters() InterrogationParameters {
 	return ip
 }
 
+// TestAskQuestionsInUnattendedAndRandomMode tests that the random mode
+// works. How can it be possible since it is random ? A simple way to check
+// this is that we are not linear. A mode complex way would be to get the
+// index of each question and check the distribution. This would be quite
+// complex test and checking the code show that we use the random function
+// if we are not linear. So this strategy should be enough.
+func TestAskQuestionsInUnattendedAndRandomMode(t *testing.T) {
+
+	r := strings.NewReader(getSampleCsvAsStream())
+	tpp := getTpp()
+	topic := ParseTopic(r, tpp)
+
+	pr, pw := io.Pipe()
+	defer pw.Close()
+	ip := getGenericUnattendedInterrogationParameters()
+  ip.out = pw
+	ip.mode = random
+
+	fmt.Println("    ****************")
+	fmt.Println("Test Ask Question in Linear Mode...")
+	fmt.Println("    ****************")
+
+	questionsSet := topic.BuildQuestionsSet()
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		AskQuestions(questionsSet, ip)
+		pw.Close()
+	}()
+
+	s := bufio.NewScanner(pr)
+
+	validateRandomOutput(tpp, questionsSet, *s, t, ip.reversed)
+}
+
 // TestAskQuestions tests that, in case of linear run of the questions,
 // you get the good questions and good answers that respects the requested
 // order.
@@ -282,7 +319,6 @@ func TestAskQuestionsInUnattendedMode(t *testing.T) {
 	s := bufio.NewScanner(pr)
 
 	validateOutput(tpp, questionsSet, *s, t, ip.reversed)
-
 }
 
 // TestAskQuestionsInReverseMode tests that, in case of linear and reverse run
@@ -349,6 +385,50 @@ func validateOutput(tpp TopicParsingParameters, questionsSet QuestionsAnswers, s
 			}
 			i = (i + 1) % questionsCount
 		}
+	}
+}
+
+//
+func validateRandomOutput(tpp TopicParsingParameters, questionsSet QuestionsAnswers, s bufio.Scanner, t *testing.T, reverseMode bool) {
+
+	announcement, _ := regexp.Compile("^" + tpp.TopicAnnounce)
+	questionsCount := questionsSet.GetCount()
+	i := 0
+	matchCount := 0
+	totalTests := 0
+	var (
+		isAnnounce     bool
+		isEmpty        bool
+		isLoop         bool
+		isSeparator    bool
+		isNbOfQ        bool
+		isLimitReached bool
+		expected       string
+		computed       string
+	)
+	for s.Scan() {
+		isAnnounce = announcement.MatchString(s.Text())
+		isEmpty = emptyLine.MatchString(s.Text())
+		isLoop = loop.MatchString(s.Text())
+		isSeparator = separator.MatchString(s.Text())
+		isNbOfQ = nbOfQuestions.MatchString(s.Text())
+		isLimitReached = limitReached.MatchString(s.Text())
+		if !isAnnounce && !isEmpty && !isLoop && !isSeparator && !isNbOfQ && ! isLimitReached {
+			// default is non reverse mode
+			expected = questionsSet.questions[i] + "     --> " + questionsSet.answers[i]
+			if reverseMode {
+			  expected = questionsSet.answers[i] + "     --> " + questionsSet.questions[i]
+			}
+			computed = s.Text()
+			if computed == expected {
+				matchCount++
+			}
+			i = (i + 1) % questionsCount
+			totalTests++
+		}
+	}
+	if matchCount == totalTests {
+		t.Errorf("The random mode does not work since we match the linear scenario.")
 	}
 }
 
